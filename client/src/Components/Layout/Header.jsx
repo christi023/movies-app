d3e0ee141913390b081adaa7476dd511eb9d77e7
Sheliann Olsson <sheliannohlsson@yahoo.com>
2020-06-29 20:09:08 +0200

    layout header

---
+++ client/src/Components/Layout/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import AuthOptions from '../Form/AuthOptions';
+
+// styles
+import './Header.css';
+export default function Header() {
+  return (
+    <header id="header">
+      <Link className="title" to="/">
+        <h1 className="title">Movies App</h1>
+      </Link>
+      <AuthOptions />
+    </header>
+  );
+}
