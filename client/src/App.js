f9000365fb5e2ade1287b70fd1cdd577098193e5
Sheliann Olsson <sheliannohlsson@yahoo.com>
2020-07-01 22:00:14 +0200

    axios, hooks, to connect to server side

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Route, Switch } from 'react-router-dom';
 // Components
 import Home from './Components/MovieHome/Home';
@@ -6,9 +6,9 @@ import Header from './Components/Layout/Header.jsx';
 import Register from './Components/Form/Register';
 import SignIn from './Components/Form/SignIn';
 //import Movie from './Components/Movie/Movie';
-
+import axios from 'axios';
 // context
-import { UserProvider } from './contexts/UserContext';
+import UserContext from './contexts/UserContext';
 //styles
 import Particles from 'react-particles-js';
 
@@ -37,21 +37,50 @@ const ParticleParams = {
 };
 
 function App() {
-  return (
-    <div className="App">
-      <>
-        <UserProvider >
-          <Particles className="particles" params={ParticleParams} />
+  const [userData, setUserData] = useState({
+    token: undefined,
+    user: undefined,
+  });
 
-          <Header />
+  useEffect(() => {
+    const checkLoggedIn = async () => {
+      let token = localStorage.getItem('auth-token');
+      if (token === null) {
+        localStorage.setItem('auth-token', '');
+        token = '';
+      }
+      const tokenRes = await axios.post('http://localhost:5000/api/users/tokenIsValid', null, {
+        headers: { 'x-auth-token': token },
+      });
+      console.log(tokenRes.data);
+
+      if (tokenRes.data) {
+        const userRes = await axios.get('http://localhost:5000/api/users/auth', {
+          headers: { 'x-auth-token': token },
+        });
+        setUserData({
+          token,
+          user: userRes.data,
+        });
+      }
+    };
+    checkLoggedIn();
+  }, []);
+
+  return (
+    <>
+      <Particles className="particles" params={ParticleParams} />
+      <UserContext.Provider value={{ userData, setUserData }}>
+        <Header />
+        <div className="App">
           <Switch>
             <Route exact path="/" render={() => <Home />} />
             <Route exact path="/register" render={() => <Register />} />
             <Route exact path="/signIn" render={() => <SignIn />} />
           </Switch>
-        </UserProvider>
-      </>
-    </div>
+        </div>
+      </UserContext.Provider>
+    </>
   );
 }
 
