df79298d60e49212a472cd89fbb4b7c9dfc93d16
Sheliann Olsson <sheliannohlsson@yahoo.com>
2020-07-01 22:01:26 +0200

    Auth options for routing

--- client/src/Components/Layout/AuthOptions.jsx
+++ client/src/Components/Layout/AuthOptions.jsx
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useHistory } from 'react-router-dom';
-
+import userContext from '../../contexts/UserContext';
 // styles
 import '../Layout/Header.css';
 
 export default function AuthOptions() {
+  const { userData, setUserData } = useContext(userContext);
+
   const history = useHistory();
 
   const register = () => history.push('/register');
   const signIn = () => history.push('/signIn');
+  const logout = () => {
+    setUserData({
+      token: undefined,
+      user: undefined,
+    });
+    localStorage.setItem(('auth-token', ''));
+  };
   return (
-    <div className="auth-options">
-      <button onClick={register}>Register</button>
-      <button onClick={signIn}>Sign In</button>
-    </div>
+    <nav className="auth-options">
+      {userData.user ? ( // if user exists returns logout button
+        <button onClick={logout}>Log Out</button>
+      ) : (
+        <>
+          <button onClick={register}>Register</button>
+          <button onClick={signIn}>Sign In</button>
+        </>
+      )}
+    </nav>
   );
 }
