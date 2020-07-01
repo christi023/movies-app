7b7578bb4b946b6e0e71e4e3ca38577039fd8d34
Sheliann Olsson <sheliannohlsson@yahoo.com>
2020-07-01 12:00:46 +0200

    Add home route

--- client/src/Components/MovieHome/Home.jsx
+++ client/src/Components/MovieHome/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-
+// resources
+//import poster1 from '../../resources/poster1.jpg';
 export default function Home() {
   return (
-    <div>
-      <Route exact path="/" />
-    </div>
+    <>
+      <div>
+        <Route exact path="/" />
+      </div>
+    </>
   );
 }
